a5518961790fcb11f321889700e3050ba3807add
carltofen1 <patrickpozsgai2@gmail.com>
2025-08-26 20:19:47 -0500

    Bucles en JS

--- JS/estructura de control condicional if.rs
+++
@@ -1,8 +0,0 @@
-let edad=20;
-if (edad) < 18{
-    console.log("eres menor de edad")
-} else if (edad >= 18 && edad < 65){
-    console.log("ees un adulto");
-} else {
-    console.log("eres una persona mayor");
-}
