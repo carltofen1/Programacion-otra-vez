a5fcf403e9e434292ad92cd36f0a1cc39405a828
carltofen1 <patrickpozsgai2@gmail.com>
2025-08-26 20:04:38 -0500

    Proyectos JS

---
+++ JS/estructura de control condicional if.rs
@@ -0,0 +1,8 @@
+let edad=20;
+if (edad) < 18{
+    console.log("eres menor de edad")
+} else if (edad >= 18 && edad < 65){
+    console.log("ees un adulto");
+} else {
+    console.log("eres una persona mayor");
+}
