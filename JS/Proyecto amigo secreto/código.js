ff5064ad2f7c166f798e667a00c96917978bc0dc
carltofen1 <patrickpozsgai2@gmail.com>
2025-09-02 17:59:10 -0500

    Mejoras para que ya sea la entrega final

--- JS/Proyecto amigo secreto/código.js
+++ JS/Proyecto amigo secreto/código.js
@@ -1,3 +1,44 @@
-let amigos = []
+let amigos = [];
 
-func (Obtener)
+function CambiarTextHtml (id, texto){
+    let elementoHTML = document.getElementById(id);
+    elementoHTML.innerHTML=texto
+}
+
+function ObtenerNombres (){
+    let Nombres = document.getElementById("amigo").value;
+        // validar que no esté vacío
+    if (Nombres === "") {
+        console.alert("Por favor, escribe un nombre");
+        return;
+    }
+    amigos.push(Nombres);
+    LimpiarCaja();
+}
+
+function SortearAmigo(){
+   let MostrarLista = document.getElementById("listaAmigos");
+   MostrarLista.innerHTML="";
+
+   if (amigos.length== 0){
+    console.alert("Por favor ingresa elementos a la lista");
+    return;
+   }
+
+   for (let i=0;i<amigos.length;i++){
+    MostrarLista.innerHTML += "<li>" + amigos[i] + "</li>";
+   }
+   let aleatorio = Math.floor(Math.random() * amigos.length);
+   resultado = (`El ganador es ${amigos[aleatorio]}`);
+   CambiarTextHtml("resultado",resultado);
+}
+
+function LimpiarCaja(){
+        document.getElementById("amigo").value = "";
+    }
+
+function MostrarLista(amigos){
+    for (let i = 0; i<amigos.length; i++){
+        i = document;
+    }
+}
